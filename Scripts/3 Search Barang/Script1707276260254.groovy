fc73424ef80efefcba15babf157ceff28bf4bb07
dharmayudhaputu <dharmayudhaputu@gmail.com>
2024-02-07 11:28:12 +0800

    push test case baru

---
+++ Scripts/3 Search Barang/Script1707276260254.groovy
@@ -0,0 +1,37 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+WebUI.openBrowser('')
+
+WebUI.navigateToUrl('https://cms.demo.katalon.com/')
+
+WebUI.click(findTestObject('Object Repository/Testing Ninja Katalon/a_Shop'))
+
+WebUI.setText(findTestObject('Object Repository/Testing Ninja Katalon/input_Search for_s'), 'Ninja')
+
+WebUI.sendKeys(findTestObject('Object Repository/Testing Ninja Katalon/input_Search for_s'), Keys.chord(Keys.ENTER))
+
+WebUI.click(findTestObject('Object Repository/Testing Ninja Katalon/a_CDATA(function()for(var gfunctiontypeof O_f8795c'))
+
+WebUI.click(findTestObject('Object Repository/Testing Ninja Katalon/img'))
+
+WebUI.closeBrowser()
+
+//belajar lagi
+
---
+++ Test Suites/3 Search Barang.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>3 Search Barang</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>27a2b7e1-0194-4d33-a9a1-4ea9f1662a30</testSuiteGuid>
+   <testCaseLink>
+      <guid>16147fe4-3f94-4ea0-b2bd-0ae98cfb97f2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/3 Search Barang</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
